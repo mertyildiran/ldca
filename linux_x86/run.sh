8a678e82901032042aa5ace3778a4fe884649eeb
Mehmet Mert Yildiran <mehmetmertyildiran@gmail.com>
2020-10-18 01:55:53 +0300

    Fix the mutate subroutine and the call orders

--- linux_x86/run.sh
+++ linux_x86/run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-for file in $(find outs/* -not -name "*.xxd");
+OUTS_DIR=outs
+
+for file in $(find ${OUTS_DIR}/* -not -name "*.xxd");
 do
     xxd "$file" > "${file}.xxd"
 done
 
-for file in outs/*.xxd
+for file in ${OUTS_DIR}/*.xxd
 do
-    git diff --no-index --color 0000000000000000000000000000000000000000000000000.xxd "$file" | cat
+    git diff --no-index --color ${OUTS_DIR}/0000000000000000000000000000000000000000000000000.xxd "$file" | cat
 done
