0bfb84d636f1abc2ef2b2c3ad08e3298dc6199c1
Mehmet Mert Yildiran <mehmetmertyildiran@gmail.com>
2020-10-20 02:45:47 +0300

    Fix the issues in the rand_byte and grow subroutines

--- linux_x86/macros.asm
+++ linux_x86/macros.asm
@@ -4,10 +4,10 @@
 ; Licensed under the GNU General Public License v2.0
 
 %macro rndNum 2
-                rdtsc                        ; Generate random bytes using CPU's clock (Read Time-Stamp Counter)
-                xor     edx, edx             ; there's no division of eax
-                mov     ecx, %2 - %1 + 1     ; possible values
-                div     ecx                  ; edx:eax / ecx --> eax quotient, edx remainder
-                mov     eax, edx             ; eax = [0, %1 - 1]
-                add     eax, %1              ; eax = [%1, %2]
+                rdtsc                           ; Generate random bytes using CPU's clock (Read Time-Stamp Counter)
+                xor     edx, edx                ; there's no division of eax
+                mov     ecx, %2 - %1 + 1        ; possible values
+                div     ecx                     ; edx:eax / ecx --> eax quotient, edx remainder
+                mov     eax, edx                ; eax = [0, %1 - 1]
+                add     eax, %1                 ; eax = [%1, %2]
 %endmacro
